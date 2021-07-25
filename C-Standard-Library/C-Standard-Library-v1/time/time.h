c571e123a633aee0cd870dd76f3d55ab317bf42a
mikechen66 <875661606@qq.com>
2021-07-25 17:58:01 +0800

    Add files via upload

--- C-Standard-Library/C-Standard-Library-v1/time/time.h
+++ C-Standard-Library/C-Standard-Library-v1/time/time.h
@@ -5,20 +5,16 @@
 #ifndef _YVALS
 #include <yvals.h>
 #endif
-
 /* macros */
-#define NULL _NULL
-#define CLOCKS_PER_SEC _CPS
-
+#define NULL    _NULL
+#define CLOCKS_PER_SEC  _CPS
 /* type definitions */
 #ifndef _SIZET
 #define _SIZET
 typedef _Sizet size_t;
-#endif 
-
+#endif
 typedef unsigned int clock_t;
 typedef unsigned long time_t;
-
 struct tm {
     int tm_sec;
     int tm_min;
@@ -29,17 +25,16 @@ struct tm {
     int tm_wday;
     int tm_yday;
     int tm_isdst;
-};
-
+    };
 /* declarations */
-char *asstime(const struct tm *);
+char *asctime(const struct tm *);
 clock_t clock(void);
 char *ctime(const time_t *);
-double difftima(time_t, time_t);
-
+double difftime(time_t, time_t);
 struct tm *gmtime(const time_t *);
 struct tm *localtime(const time_t *);
-time_t mktime (struct tm *);
-size_t strftime(char *, size_t, const char *, const struct tm *);
+time_t mktime(struct tm *);
+size_t strftime(char *, size_t, const char *,
+    const struct tm *);
 time_t time(time_t *);
 #endif
