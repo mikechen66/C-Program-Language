bb409058cb439fe1d80ff33910075d59a288b688
mikechen66 <875661606@qq.com>
2021-07-25 17:45:21 +0800

    Add files via upload

--- C-Standard-Library/C-Standard-Library-v1/setjmp/setjmp.h
+++ C-Standard-Library/C-Standard-Library-v1/setjmp/setjmp.h
@@ -5,10 +5,10 @@
 #ifndef _YVALS
 #include <yvals.h>
 #endif
-/* macros */
+        /* macros */
 #define setjmp(env) _Setjmp(env)
-/* type definitions */
+        /* type definitions */
 typedef int jmp_buf[_NSETJMP];
-/* declarations */
+        /* declarations */
 void longjmp(jmp_buf, int);
 #endif
