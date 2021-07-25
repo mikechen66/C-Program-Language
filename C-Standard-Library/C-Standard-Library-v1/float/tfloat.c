eeffd0b15003aad980ece9cba457423099654dde
mikechen66 <875661606@qq.com>
2021-07-25 17:03:01 +0800

    Add files via upload

--- C-Standard-Library/C-Standard-Library-v1/float/tfloat.c
+++ C-Standard-Library/C-Standard-Library-v1/float/tfloat.c
@@ -6,40 +6,40 @@
 #include <stdio.h>
 
 int main()
-{   /* test basic properties of f1oat.h macros */
-    double radlcg;
+{   /* test basic properties of float.h macros */
+    double radlog;
     int digs;
     static int radix = FLT_RADIX;
 
     printf("FLT_RADIX = %i\n\n", FLT_RADIX);
-    printf("DBL_DIG = %5i DBL_MANT_DIG = %6i\n", 
+    printf("DBL_DIG =        %5i   DBL_MANT_DIG =  %6i\n",
         DBL_DIG, DBL_MANT_DIG);
-    printf("DBL_MAX_10_EXP = %5i DBL_MAX_EXP = %6i\n",
-        DBL_10_EXP, DBL_MAX_EXP);
-    printf("DBL_MIN_1O_EXP = %5i DBL_MIN_EXP = %6i\n",
+    printf("DBL_MAX_10_EXP = %5i   DBL_MAX_EXP =   %6i\n",
+        DBL_MAX_10_EXP, DBL_MAX_EXP);
+    printf("DBL_MIN_10_EXP = %5i   DBL_MIN_EXP =   %6i\n",
         DBL_MIN_10_EXP, DBL_MIN_EXP);
-    printf("    DBL_EPSILON = %1e\n", DBL_EPSILON);
-    printf("    DBL_MAX = %1e\n",     DBL_MAX);
-    printf("    DBL_MIN = %le\n\n",   DBL_MIN);
-    printf("FLT_DIG = %5i FLT_MANT_DIG = %6i\n",
+    printf("      DBL_EPSILON =  %le\n", DBL_EPSILON);
+    printf("      DBL_MAX =      %le\n", DBL_MAX);
+    printf("      DBL_MIN =      %le\n\n", DBL_MIN);
+    printf("FLT_DIG =        %5i   FLT_MANT_DIG =  %6i\n",
         FLT_DIG, FLT_MANT_DIG);
-    printf("FLT_MAX_10_EXP = %5i FLT_EXP = %6i\n",
-        FLT_10_EXP, FLT_MAX_EXP);
-    printf("FLT_MIN_10_EXP = %5i FLT_MIN_EXP = %6i\n",
+    printf("FLT_MAX_10_EXP = %5i   FLT_MAX_EXP =   %6i\n",
+        FLT_MAX_10_EXP, FLT_MAX_EXP);
+    printf("FLT_MIN_10_EXP = %5i   FLT_MIN_EXP =   %6i\n",
         FLT_MIN_10_EXP, FLT_MIN_EXP);
-    printf("    FLT_EPSILON = %e\n", FLT_EPSILON);
-    printf("    FLT_MAX = %e\n" ,    FLT_MAX);
-    printf("    FLT_MIN = %e\n\n",   FLT_MIN);
-    printf("LDBL_DIG = %5i LDBL_MANT_DIG = %6i\n",
+    printf("      FLT_EPSILON =  %e\n", FLT_EPSILON);
+    printf("      FLT_MAX =      %e\n", FLT_MAX);
+    printf("      FLT_MIN =      %e\n\n", FLT_MIN);
+    printf("LDBL_DIG =        %5i  LDBL_MANT_DIG = %6i\n",
         LDBL_DIG, LDBL_MANT_DIG);
-    printf("LDBL_MAX_1O_EXP = %5i LDBL_MAX_EXP = %6i\n",
-        LDBL_10_EXP, LDBL_MAX_EXP);
-    printf("LDBL_MIN_10_EXP = %5i LDBL_MIN_EXP = %6i\n",
+    printf("LDBL_MAX_10_EXP = %5i  LDBL_MAX_EXP =  %6i\n",
+        LDBL_MAX_10_EXP, LDBL_MAX_EXP);
+    printf("LDBL_MIN_10_EXP = %5i  LDBL_MIN_EXP =  %6i\n",
         LDBL_MIN_10_EXP, LDBL_MIN_EXP);
-    printf("    LDBL_EPSILON = %Le\n", LDBL_EPSILON);
-    printf("    LDBL_MAX =     %Le\n", LDBL_MAX);
-    printf("    LDBL_MIN =     %Le\n", LDBL_MIN);
-    radlog = log1O(radix);
+    printf("      LDBL_EPSILON = %Le\n", LDBL_EPSILON);
+    printf("      LDBL_MAX =     %Le\n", LDBL_MAX);
+    printf("      LDBL_MIN =     %Le\n", LDBL_MIN);
+    radlog = log10(radix);
     /* test double properties */
     assert(10 <= DBL_DIG && FLT_DIG <= DBL_DIG);
     assert(DBL_EPSILON <= 1e-9);
@@ -48,7 +48,7 @@ int main()
     assert(1e37 <= DBL_MAX);
     assert(37 <= DBL_MAX_10_EXP);
 #if FLT_RADIX == 2
-    assert(ldexp(1.0, DBL_EXP - 1) < DBL_MAX);
+    assert(ldexp(1.0, DBL_MAX_EXP - 1) < DBL_MAX);
     assert(ldexp(1.0, DBL_MIN_EXP - 1) == DBL_MIN);
 #endif
     assert(DBL_MIN <= 1e-37);
@@ -59,10 +59,10 @@ int main()
     digs = (FLT_MANT_DIG - 1) * radlog;
     assert(digs <= FLT_DIG && FLT_DIG <= digs + 1);
     assert(1e37 <= FLT_MAX);
-    assert(37 <= FLT_10_EXP);
+    assert(37 <= FLT_MAX_10_EXP);
 #if FLT_RADIX == 2
-    assert(ldexp(1.0, FLT_MAX_EXP - 1) < FLT_MAX)
-    assert(ldexp(1.0, FLT_MIN_EXP - 1) == FLT_MIN); 
+    assert(ldexp(1.0, FLT_MAX_EXP - 1) < FLT_MAX);
+    assert(ldexp(1.0, FLT_MIN_EXP - 1) == FLT_MIN);
 #endif
     assert(FLT_MIN <= 1e-37);
     assert(FLT_MIN_10_EXP <= -37);
@@ -81,7 +81,7 @@ int main()
 #if FLT_RADIX == 2
     assert(DBL_MAX_EXP < LDBL_MAX_EXP
         || ldexp(1.0, LDBL_MAX_EXP - 1) < LDBL_MAX);
-    aeeert(LDBL_MIN_EXP < DBL_MIN_EXP
+    assert(LDBL_MIN_EXP < DBL_MIN_EXP
         || ldexp(1.0, LDBL_MIN_EXP - 1) == LDBL_MIN);
 #endif
     assert(LDBL_MIN <= 1e-37);
