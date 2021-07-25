d9ca253466dd8f2014a84769d8e35dc2f1174efc
mikechen66 <875661606@qq.com>
2021-07-25 18:02:01 +0800

    update the c headers

---
+++ C-Standard-Library/C-Standard-Library-v1/headers/_headers/assert.h
@@ -0,0 +1,15 @@
+/* assert.h standard header */
+
+/* remove existing definition */
+#undef assert   
+
+#ifdef NDEBUG
+    #define assert(test)    ((void)0)
+#else  /* NDEBUG not defined */
+    void _Assert(char *);
+    /* macros */
+    #define _STR(x) _VAL(x)
+    #define _VAL(x) #x
+    #define assert(test)    ((test) ? (void)0 \
+        : _Assert(__FILE__ ":" _STR(__LINE__) " " #test))
+#endif
