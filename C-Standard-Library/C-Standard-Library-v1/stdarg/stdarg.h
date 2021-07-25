43257796f6b43de423bbd375c7ce3766a152db42
mikechen66 <875661606@qq.com>
2021-07-25 17:48:40 +0800

    Add files via upload

--- C-Standard-Library/C-Standard-Library-v1/stdarg/stdarg.h
+++ C-Standard-Library/C-Standard-Library-v1/stdarg/stdarg.h
@@ -3,17 +3,15 @@
 #ifndef _STDARG
 #define _STDARG
 #ifndef _YVALS
-#include <pals.h>
+#include <yvals.h>
 #endif
-
 /* type definitions */
-typedef char *va-list;
-
+typedef char *va_list;
 /* macros */
-#define va_arg(ap, T) \
+#define va_arg(ap, T)   \
     (*(T *)(((ap) += _Bnd(T, _AUPBND)) - _Bnd(T, _ADNBND)))
-#define va_end(ap) (void) 0
+#define va_end(ap)      (void)0
 #define va_start(ap, A) \
-    (void)((ap) = (char *) & (A) + _bnd(A, _AUPBND))
-#define _Bnd(X, bnd) (sizeof(X) + (bnd) & - (bnd))
+    (void)((ap) = (char *)&(A) + _Bnd(A, _AUPBND))
+#define _Bnd(X, bnd)    (sizeof (X) + (bnd) & ~(bnd))
 #endif
