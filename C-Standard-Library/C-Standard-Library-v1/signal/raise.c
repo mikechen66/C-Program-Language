3a55f9875f2e401f0e77d3ac8fe9d99d8f6e8e90
mikechen66 <875661606@qq.com>
2021-07-26 23:34:18 +0800

    Add files via upload

--- C-Standard-Library/C-Standard-Library-v1/signal/raise.c
+++ C-Standard-Library/C-Standard-Library-v1/signal/raise.c
@@ -14,44 +14,43 @@ int(raise)(int sig)
     if (sig <= 0 || _NSIG <= sig)
         return (-1);    /* bad signal */
     if ((s = _Sigtable[sig]) != SIG_IGN && s != SIG_DFL)
-        {   /* revert and call handler */
+    {   /* revert and call handler */
         _Sigtable[sig] = SIG_DFL;
         (*s)(sig);
-        }
+    }
     else if (s == SIG_DFL)
-        {   /* default handling */
+    {   /* default handling */
         char ac[10], *p;
 
         switch (sig)
-            {   /* print known signals by name */
-        case SIGABRT:
-            p = "abort";
-            break;
-        case SIGFPE:
-            p = "arithmetic error";
-            break;
-        case SIGILL:
-            p = "invalid executable code";
-            break;
-        case SIGINT:
-            p = "interruption";
-            break;
-        case SIGSEGV:
-            p = "invalid storage access";
-            break;
-        case SIGTERM:
-            p = "termination request";
-            break;
-        default:
-            *(p = &ac[(sizeof ac) - 1]) = '\0';
-            do *--p = sig % 10 + '0';
-                while ((sig /= 10) != 0);
-            fputs("signal #", stderr);
-            }
+        {   /* print known signals by name */
+            case SIGABRT:
+                p = "abort";
+                break;
+            case SIGFPE:
+                p = "arithmetic error";
+                break;
+            case SIGILL:
+                p = "invalid executable code";
+                break;
+            case SIGINT:
+                p = "interruption";
+                break;
+            case SIGSEGV:
+                p = "invalid storage access";
+                break;
+            case SIGTERM:
+                p = "termination request";
+                break;
+            default:
+                *(p = &ac[(sizeof ac) - 1]) = '\0';
+                do *--p = sig % 10 + '0';
+                    while ((sig /= 10) != 0);
+                fputs("signal #", stderr);
+        }
         fputs(p, stderr);
         fputs(" -- terminating\n", stderr);
         exit(EXIT_FAILURE);
-        }
+    }
     return (0);
 }
-
