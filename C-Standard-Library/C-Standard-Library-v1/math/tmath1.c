e065dd4f35b587a065c42dc1f0a77b2b7d0ea441
mikechen66 <875661606@qq.com>
2021-07-26 23:10:38 +0800

    Add files via upload

--- C-Standard-Library/C-Standard-Library-v1/math/tmath1.c
+++ C-Standard-Library/C-Standard-Library-v1/math/tmath1.c
@@ -1,4 +1,4 @@
-/* test math functions — part 1 */
+/* test math functions -- part 1 */
 
 #include <assert.h>
 #include <float.h>
@@ -6,100 +6,64 @@
 #include <stdio.h>
 
 static double eps;
-static int approx(double dl, double d2)
-{ /* test for approximate equality */
+
+static int approx(double d1, double d2)
+{   /* test for approximate equality */
     if (d2 != 0)
-        return (fabs((d2 - dl) / d2) < eps);
+        return (fabs((d2 - d1) / d2) < eps);
     else
-        return (fabs(dl) < eps);
+        return (fabs(d1) < eps);
 }
 
 int main()
-{ /* test basic workings of math functions */
-double huge_val, x;
-int xexp;
-huge_val = HUGE_VAL;
-eps = DBL_EPSILON * 4.0;
-assert(ceil(-5.1) = -5.0);
-assert(ceil(-5.0) = -5.0);
-assert (ceil (-4.9) = -4.0);
-assert (ceil @.0) = 0.0);
-assert (ceilD.9) = 5.0);
-assert (ceilE.0) = 5.0);
-assert(ceilE.1) = 6.0);
-assert(fabs(-5.0) = 5.0);
-assert (fabs@.0) = 0.0);
-assert (fabs E.0) =5.0);
-assert(floor(-5.1) == -6.0);
-assert(floor(-5.0) == -5.0);
-assert(floor(-4.9) == -5.0);
-assert(floor@.0) = 0.0);
-assert(floorD.9) = 4.0);
-assert(floorE.0) = 5.0);
-assert(floorE.1) = 5.0);
-assert(?mod(-7.0, 3.0) =
-assert(?mod(-3.0, 3.0) =
-assert(?mod(-2.0, 3.0) =
-assert(?mod@.0, 3.0) =
-assert(?modB.0, 3.0) =
-assert(fmodC.0, 3.0) ==
-assert(?modG.0, 3.0) =
-assert(approx(frexp(-3.0
-assert(approx(frexp(-0.5
-assert(frexp@.0, fixexp)
-assert(approx(frexp@.33, fixexp)
-assert(approx(frexp@.66, fixexp)
-assert(approx(frexp(96.0, fixexp)
-assert(ldexp(-3.0, 4) = -48.0);
-assert(ldexp(-0.5, 0) = -0.5);
-' -1-0);
-' 0.0);
-¦¦ -2.0);
-0.0);
-2.0);
-0.0);
-1.0);
-fixexp), -0.75) 66 xexp
-fixexp), -0.5) 66 xexp =
-= 0.0 && xexp = 0);
-0.66) && xexp =
-0.66) 66 xexp =
-0.75)
+{   /* test basic workings of math functions */
+    double huge_val, x;
+    int xexp;
 
-
-assert(ldexp@.0, 36) =
-assert(approx(ldexp@.66
-assert (ldexp (96, -3) ==
-assert(approx(modf(-11.7, &x), -11.7 + 11.0)
-    && x = -11.0);
-assert(modf(-0.5, fix) =
-assert(modf@.0, fix) ==
-assert (modf @.6, fix) =
-assert(modfA2.0, fix) =
-printf("HUGE_VAL prints as %.16e\n", huge val);
-return (0);
+    huge_val = HUGE_VAL;
+    eps = DBL_EPSILON * 4.0;
+    assert(ceil(-5.1) == -5.0);
+    assert(ceil(-5.0) == -5.0);
+    assert(ceil(-4.9) == -4.0);
+    assert(ceil(0.0) == 0.0);
+    assert(ceil(4.9) == 5.0);
+    assert(ceil(5.0) == 5.0);
+    assert(ceil(5.1) == 6.0);
+    assert(fabs(-5.0) == 5.0);
+    assert(fabs(0.0) == 0.0);
+    assert(fabs(5.0) == 5.0);
+    assert(floor(-5.1) == -6.0);
+    assert(floor(-5.0) == -5.0);
+    assert(floor(-4.9) == -5.0);
+    assert(floor(0.0) == 0.0);
+    assert(floor(4.9) == 4.0);
+    assert(floor(5.0) == 5.0);
+    assert(floor(5.1) == 5.0);
+    assert(fmod(-7.0, 3.0) == -1.0);
+    assert(fmod(-3.0, 3.0) == 0.0);
+    assert(fmod(-2.0, 3.0) == -2.0);
+    assert(fmod(0.0, 3.0) == 0.0);
+    assert(fmod(2.0, 3.0) == 2.0);
+    assert(fmod(3.0, 3.0) == 0.0);
+    assert(fmod(7.0, 3.0) == 1.0);
+    assert(approx(frexp(-3.0, &xexp), -0.75) && xexp == 2);
+    assert(approx(frexp(-0.5, &xexp), -0.5) && xexp == 0);
+    assert(frexp(0.0, &xexp) == 0.0 && xexp == 0);
+    assert(approx(frexp(0.33, &xexp), 0.66) && xexp == -1);
+    assert(approx(frexp(0.66, &xexp), 0.66) && xexp == 0);
+    assert(approx(frexp(96.0, &xexp), 0.75) && xexp == 7);
+    assert(ldexp(-3.0, 4) == -48.0);
+    assert(ldexp(-0.5, 0) == -0.5);
+    assert(ldexp(0.0, 36) == 0.0);
+    assert(approx(ldexp(0.66, -1), 0.33));
+    assert(ldexp(96, -3) == 12.0);
+    assert(approx(modf(-11.7, &x), -11.7 + 11.0)
+        && x == -11.0);
+    assert(modf(-0.5, &x) == -0.5 && x == 0.0);
+    assert(modf(0.0, &x) == 0.0 && x == 0.0);
+    assert(modf(0.6, &x) == 0.6 && x == 0.0);
+    assert(modf(12.0, &x) == 0.0 && x == 12.0);
+    printf("HUGE_VAL prints as %.16e\n", huge_val);
+    puts("SUCCESS testing <math.h>, part 1");
+    return (0);
 }
-
-
-, -1), 0.33)
-12.0);
-, fix), -11.7
--0.5 fifi x =
-0.0 fifi x ==
-0.666 x = 0
-0.066 x =
-
-puts("SUCCESS testing <math.h>, part
-return @) ;
-) ;
-+ 11.0)
-= 0.0);
-0.0);
-•0);
-12.0);
-
-1");
-
-
-
-
