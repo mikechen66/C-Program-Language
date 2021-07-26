05741e5b562f7256224b58fe6ba16696f92064c4
mikechen66 <875661606@qq.com>
2021-07-26 21:43:12 +0800

    Add files via upload

--- C-Standard-Library/C-Standard-Library-v1/math/xdnit.c
+++ C-Standard-Library/C-Standard-Library-v1/math/xdnit.c
@@ -16,21 +16,21 @@ short _Dint(double *px, short xexp)
     else if (!frac)
         return (INF);
     else
-        {   /* NaN */
+    {   /* NaN */
         errno = EDOM;
         return (NAN);
-        }
+    }
     xchar = (_DBIAS+48+_DOFF+1) - xchar - xexp;
     if (xchar <= 0)
         return (0); /* no frac bits to drop */
     else if ((48+_DOFF) < xchar)
-        {   /* all frac bits */
+    {   /* all frac bits */
         ps[_D0] = 0, ps[_D1] = 0;
         ps[_D2] = 0, ps[_D3] = 0;
         return (FINITE);
-        }
+    }
     else
-        {   /* strip out frac bits */
+    {   /* strip out frac bits */
         static const unsigned short mask[] = {
             0x0000, 0x0001, 0x0003, 0x0007,
             0x000f, 0x001f, 0x003f, 0x007f,
@@ -43,14 +43,14 @@ short _Dint(double *px, short xexp)
         frac &= ps[sub[xchar]];
         ps[sub[xchar]] ^= frac;
         switch (xchar)
-            {   /* cascade through! */
-        case 3:
-            frac |= ps[_D1], ps[_D1] = 0;
-        case 2:
-            frac |= ps[_D2], ps[_D2] = 0;
-        case 1:
-            frac |= ps[_D3], ps[_D3] = 0;
-            }
-        return (frac ? FINITE : 0);
+        {   /* cascade through! */
+            case 3:
+                frac |= ps[_D1], ps[_D1] = 0;
+            case 2:
+                frac |= ps[_D2], ps[_D2] = 0;
+            case 1:
+                frac |= ps[_D3], ps[_D3] = 0;
         }
+        return (frac ? FINITE : 0);
+    }
 }
