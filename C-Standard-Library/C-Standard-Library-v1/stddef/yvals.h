0aa4f1b47d7141c5119fc9c1588945b5a9014709
mikechen66 <875661606@qq.com>
2021-07-27 14:16:47 +0800

    Add the file of yvals.h in compliance with VAX

---
+++ C-Standard-Library/C-Standard-Library-v1/stddef/yvals.h
@@ -0,0 +1,54 @@
+/* yvals.h values header -- VAX ULTRIX version */
+
+/* Please notice that there are three files of yvals.h r
+ * respectively distributed in _unixvax (for the ULTRIX 
+ * of DEC acquired by HP), _unix68k for Sun UNIX (690X0)
+ * and _tc86 for Turbo C++ compiler (or ANSI C). The three
+ * files are different in a subtle way. 
+ */ 
+
+#define _YVALS
+/* errno properties */
+#define _EDOM   33
+#define _ERANGE 34
+#define _EFPOS  35
+#define _ERRMAX 36
+/* float properties */
+#define _D0     0
+#define _DBIAS  0x80
+#define _DLONG  0
+#define _DOFF   7
+#define _FBIAS  0x80
+#define _FOFF   7
+#define _FRND   1
+#define _LBIAS  0x80
+#define _LOFF   7
+/* integer properties */
+#define _C2     1
+#define _CSIGN  1
+#define _ILONG  1
+#define _MBMAX  8
+typedef unsigned short _Wchart;
+/* pointer properties */
+#define _NULL   (void *)0
+typedef int _Ptrdifft;
+typedef unsigned int _Sizet;
+/* setjmp properties */
+#define _NSETJMP    80
+int _Setjmp(int *);
+/* signal properties */
+#define _SIGABRT    6
+#define _SIGMAX     32
+/* stdio properties */
+#define _FNAMAX 64
+#define _FOPMAX 16
+#define _TNAMAX 16
+/* stdlib properties */
+#define _EXFAIL 1
+/* storage alignment properties */
+#define _AUPBND 3U
+#define _ADNBND 3U
+#define _MEMBND 7U
+/* time properties */
+#define _CPS    1
+#define _TBIAS  0
