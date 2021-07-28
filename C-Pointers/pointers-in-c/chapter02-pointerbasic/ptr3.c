c8aa2f12e9a656a1af241b8e6f583c7a5b02c5f2
mikechen66 <875661606@qq.com>
2021-07-28 17:55:52 +0800

    Add files via upload

---
+++ C-Pointers/pointers-in-c/chapter02-pointerbasic/ptr3.c
@@ -0,0 +1,13 @@
+struct node{   
+    int a;     
+    int b;
+};
+
+int  main()   
+{
+    struct node p;
+    printf("Address of node = %p\n",&p);
+    printf("Address of member variable a = %p\n", &(p.a));
+    printf("Address of member variable b = %p\n", &(p.b));
+    return 0;
+}
---
+++ C-Pointers/pointers-in-c/chapter04-pointerforstring/string10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <malloc.h>
+
+int main()
+{                                                       
+    char arrstr[6];                                             
+    char* strptr;                                           
+    printf(“Input hello\n”);                                
+    scanf(“%s”, arrstr);                                            
+    strptr = arrstr;                                        
+    while(*strptr != ‘\0’)                                      
+    {                                                       
+        printf(“%c”,*strptr);                                           
+        strptr++;                                       
+    } 
+}
